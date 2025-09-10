46e04794e8a12efbfe38e2fae2edf9dc6c351509
Dinesh Sake <88959317+Nrupatungan@users.noreply.github.com>
2025-09-10 16:10:56 +0530

    Update profile picture src

--- client/app/(components)/Navbar/index.tsx
+++ client/app/(components)/Navbar/index.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
           <hr className='w-0 h-7 border-solid border border-gray-300 mx-3' />
           <div className='flex items-center gap-3 cursor-pointer'>
             <Image
-              src="https://s3-inventorybucket.s3.us-east-1.amazonaws.com/profile.jpg"
+              src="https://d-stock-inventory.s3.us-east-1.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
